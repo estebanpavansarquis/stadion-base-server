e40d2f11dcacc2297f756eb3a34cbe8f257911c1
estebanpavansarquis <estebanpavansarquis@gmail.com>
2020-11-07 16:48:10 -0300

    lcd display controller added
    Signed-off-by: estebanpavansarquis <estebanpavansarquis@gmail.com>

---
+++ controllers/lcd_display_controller.py
@@ -0,0 +1,24 @@
+
+import drivers.lcddriver as lcddriver
+import time
+
+class LCDDisplayController:
+
+    def __init__(self):
+        self.dispaly = lcddriver.lcd()
+
+    def displayWelcomeMsg(self):
+        self.display.lcd_clear()
+        self.display.lcd_display_string("      ENJOY      ", 1) 
+        self.display.lcd_display_string("     STADION     ", 2)
+        time.sleep(1)
+
+    def displayWaitingForConnectionsMsg(self):
+        self.display.lcd_clear()
+        self.display.lcd_display_string("Connect BTH to", 1) 
+        self.display.lcd_display_string("ID: stadion", 2)
+
+    def clearDisplay(seld):
+        self.display.lcd_clear()
+
+
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from controllers.bluetooth_controller import BluetoothController
+from controllers.lcd_display_controller import LCDDisplayController
+
 
 def main():
     while True:
+        display_controller = LCDDisplayController()
+
+        display_controller.displayWaitingForConnectionsMsg()
         bluetooth_controller = BluetoothController()
-        
+           
         while bluetooth_controller.isConnected():
 
             received_data = bluetooth_controller.read()
